e0bdf2fced53c3596956f515bafc6255b5be174d
lingyuan-yousa <365474180@qq.com>
2023-06-13 21:05:26 +0800

    创建项目框架

---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerAplication.java
@@ -0,0 +1,11 @@
+package com.mashibing.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPassengerAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerAplication.class);
+    }
+}
