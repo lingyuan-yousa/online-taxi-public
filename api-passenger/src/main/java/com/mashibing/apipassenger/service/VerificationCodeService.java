c55b3b1379f8cd924f4f831a1b78a196efdd5a7a
lingyuan-yousa <365474180@qq.com>
2023-06-26 17:38:34 +0800

    数据库操作

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,9 +1,12 @@
 package com.mashibing.apipassenger.service;
 
 import com.mashibing.apipassenger.remote.ServiceVerificationcodeClient;
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.NumberCodeResponse;
+import com.mashibing.internalcommon.response.TokenResponse;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -22,6 +25,12 @@ public class VerificationCodeService {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    /**
+     * 生成验证码
+     * @param passengerPhone 手机号
+     * @return
+     */
+
     public ResponseResult generatorCode(String passengerPhone) {
 
         // 调用验证码服务 获取验证码
@@ -29,7 +38,7 @@ public class VerificationCodeService {
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
         // key, value, 过期时间
-        String key = verificationCodePrefix + passengerPhone;
+        String key = generatorKeyByPhone(passengerPhone);
 
         // 存入redis
         stringRedisTemplate.opsForValue().set(key, numberCode + "", 2, TimeUnit.MINUTES);
@@ -38,5 +47,52 @@ public class VerificationCodeService {
 
 
         return ResponseResult.success("");
+    }
+
+    /**
+     * 根据手机号生成key
+     * @param passengerPhone
+     * @return
+     */
+    private String generatorKeyByPhone(String passengerPhone) {
+        return verificationCodePrefix + passengerPhone;
+    }
+    
+    /**
+     * 校验验证码
+     * @param passengerPhone 手机号
+     * @param verificationCode 验证码
+     * @return
+     */
+    public ResponseResult checkCode(String passengerPhone, String verificationCode) {
+        // 根据手机号，去redis读取验证码
+        System.out.println("根据手机号， 去redis读取验证码");
+
+        // 生成key
+        String key = generatorKeyByPhone(passengerPhone);
+
+        // 根据key获取value
+        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("redis中的value: " + codeRedis);
+
+        // 校验验证码
+        if (StringUtils.isBlank(codeRedis)) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+
+        if (!verificationCode.trim().equals(codeRedis.trim())) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+
+        // 判断原来是否有用户，并进行对应的处理
+        System.out.println("判断原来是否有用户，并进行对应的处理");
+
+        // 颁发令牌
+        System.out.println("颁发令牌");
+
+        // 响应
+        TokenResponse tokenResponse = new TokenResponse();
+        tokenResponse.setToken("token value");
+        return ResponseResult.success(tokenResponse);
     }
 }
